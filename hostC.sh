097ef7f28441d51b1c47b299c4494a4b308d68ec
Simone Luchetta <simoneluchetta@MacBook-Pro-di-Simone.local>
2019-10-28 14:18:04 +0100

    added configurations for nginx

--- hostC.sh
+++ hostC.sh
@@ -1,8 +1,19 @@
 export DEBIAN_FRONTEND=noninteractive
 
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+# docker pull nginx
+docker pull dustnic82/nginx-test
+docker run --name dustnic82 -v /simoneluchetta:/usr/share/nginx/html:ro -d nginx
+# docker run --dustnic82/nginx-test -d -p 8080:80 simoneluchetta
+
 # COLLEGAMENTO DALL'HOST-C AL ROUTER-2
 ip addr add 172.16.2.23/23 dev enp0s8
 ip link set enp0s8 up
 
-# Default GateWays
+# Default Gateway
 ip route add default via 172.16.2.1
--- router1.sh
+++ router1.sh
@@ -16,7 +16,7 @@ ip link add link enp0s8 name enp0s8.8 type vlan id 8
 ip addr add 172.16.1.1/24 dev enp0s8.8
 ip link set enp0s8.8 up
 
-#COLLEGAMENTO DAL ROUTER-1 AL ROUTER-2
+# COLLEGAMENTO DAL ROUTER-1 AL ROUTER-2
 ip addr add 172.16.4.1/30 dev enp0s9 
 ip link set enp0s9 up
 
