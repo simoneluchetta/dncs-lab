7921e2eb6a74cdaed83b0c6e0aeb14c12b207a44
Simone Luchetta <simoneluchetta@MacBook-Pro-di-Simone.local>
2019-11-05 17:13:41 +0100

    added a properly almost working method 2

--- hostC.sh
+++ hostC.sh
@@ -6,36 +6,33 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
-#sudo systemctl start docker
-#sudo systemctl enable docker
 
-# Metodo 1
-docker pull dustnic82/nginx-test
-mkdir ~/DNCS_Lab_Simo 
-cd ~/DNCS_Lab_Simo
-touch index.html
-printf '<!DOCTYPE html> <html lang="en"> <head> <title> DNCS_LAB </title> </head> <body> <p>Hello World!<p> </body> </html>' > index.html
-docker run --name method1nginx -v ~/DNCS_Lab_Simo:/usr/share/nginx/html:ro -d -p 80:80 dustnic82/nginx-test
+# Metodo 1, Funzionante:
+#docker pull dustnic82/nginx-test
+#mkdir ~/DNCS_Lab_Simo 
+#cd ~/DNCS_Lab_Simo
+#touch index.html
+#printf '<!DOCTYPE html> <html lang="en"> <head> <title> DNCS_LAB </title> </head> <body> <p>Hello World!<p> </body> </html>' > index.html
+#docker run --name method1nginx -v ~/DNCS_Lab_Simo:/usr/share/nginx/html:ro -d -p 80:80 dustnic82/nginx-test
 
 
 # Metodo 2
-#mkdir ~/myfolder
-#cd ~/myfolder
 
-#echo 'FROM ubuntu 
-#RUN apt-get update 
-#RUN apt-get install nginx -y 
-#COPY ~/myfolder/index.html /var/www/html
-#EXPOSE 80 
-#CMD ["nginx","-g","daemon off;"]' > Dockerfile
+mkdir ~/DNCS_Lab_Simo 
+cd ~/DNCS_Lab_Simo
 
-#echo '<html> 
-#<h1>Hello World!</h1> 
-#<p>This is a simple static website!</p> 
-#</html>' > index.html
+touch index.html
+printf '<!DOCTYPE html> <html lang="en"> <head> <title> DNCS_LAB </title> </head> <body> <p>Hello World!<p> </body> </html>' > index.html
 
-#sudo docker build -f ~/myfolder/Dockerfile .
-#docker run -d ~/myfolder/Dockerfile --name method2nginx -p 7000:80 nginx
+touch Dockerfile
+printf 'FROM dustnic82/nginx-test
+WORKDIR /usr/share/nginx/html
+COPY index.html /usr/share/nginx/html
+EXPOSE 80
+' > Dockerfile
+
+docker build -t ciro .
+docker run --name method2nginx ciro
 
 # COLLEGAMENTO DALL'HOST-C AL ROUTER-2
 ip addr add 172.16.2.23/23 dev enp0s8
